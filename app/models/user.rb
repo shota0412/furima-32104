c95b69bca10bdda7b44ff60f6d04dde95b7f67e9
shota0412 <sh.v.w0412@gmail.com>
2020-12-09 15:46:33 +0900

    userテーブルの実装、viewファイルの配置

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  validates :encrypted_password, format: {with: PASSWORD_REGEX}
+  validates :encrypted_password_confirmation, presence: true
+  
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true, format: {with: NAME_REGEX}
+  validates :last_name, presence: true, format: {with: NAME_REGEX}
+
+  FURIGANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :first_name_furigana, presence: true, format: {with: FURIGANA_REGEX}
+  validates :last_name_furigana, presence: true, format: {with: FURIGANA_REGEX}
+  validates :birthday, presence: true    
+end
