ce427840819af9c6ba91034c70aa877c5f51bd66
shota0412 <sh.v.w0412@gmail.com>
2020-12-14 00:01:38 +0900

    Active Strageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
