c1b8e2eb0a9065f50048d7f8933c13e9ef434244
shota0412 <sh.v.w0412@gmail.com>
2020-12-15 20:09:42 +0900

    テストコード、item_price.js、itemコントローラーの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,16 +2,13 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(addTaxDom);
+
     const addTaxValue = inputValue * 0.1
-    console.log(addTaxValue)
     
     const profitOutput = document.getElementById("profit");
     profitOutput.innerHTML = (Math.floor(inputValue - addTaxValue));
-    console.log(profitOutput)
   })
 });
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,10 +69,30 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-      it 'Active Hashのidが1では登録できない' do
+      it 'category_idが1では登録できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'status_idが1では登録できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it 'delivery_cost_idが1では登録できない' do
+        @item.delivery_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost must be other than 1")
+      end
+      it 'prefecture_idが1では登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'delivery_date_idが1では登録できない' do
+        @item.delivery_date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery date must be other than 1")
       end
     end
   end
