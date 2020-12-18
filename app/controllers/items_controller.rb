69a070f640a761a7ac28ee913be5da0228d8ad0e
shota0412 <sh.v.w0412@gmail.com>
2020-12-18 19:22:20 +0900

    updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,21 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path 
+    end
+    unless @item.order.blank?
+      redirect_to new_user_session_path 
+    end 
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
