ee537def420a5013e786c860202ec723a51e59a8
shota0412 <sh.v.w0412@gmail.com>
2020-12-19 20:59:33 +0900

    destroyアクション失敗時の遷移の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,9 @@ class ItemsController < ApplicationController
   def destroy
     if current_user.id == @item.user_id 
       if @item.destroy
-      redirect_to root_path
+        redirect_to root_path
+      else
+        redirect_to item_path
       end
     else
       redirect_to item_path
