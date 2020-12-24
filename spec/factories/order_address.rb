1424fbda293d34b437506b2dadeb94af80611b73
shota0412 <sh.v.w0412@gmail.com>
2020-12-24 18:49:21 +0900

    テストコードの実装

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :order_address do
     zip              { '111-2222' }
-    prefecture_id       { 2 }
+    prefecture_id    { 2 }
     city             { "テスト町" }
     address          { "テスト1-1-1" }
     building         { "テスト101" }
     phone_number     { '09011112222' }
+    token            {'tk_test_000000000000000'}
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -52,6 +52,11 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number is invalid")
       end
+      it 'tokenが空だと購入できない' do
+        @order_address.token = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
